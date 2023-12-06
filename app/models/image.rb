746dc536c9c8cad6ef55ff5cd67ba0293b7d924e
Rush <jwrush@gmail.com>
2023-12-05 21:08:04 -0600

    first linux commit

---
+++ app/models/image.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# The Image class is used to represent a directory on disk.
+class Image
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def caption_exists?
+    File.exist?(caption_path)
+  end
+
+  def caption_path
+    "#{path}.txt"
+  end
+
+  def caption
+    return nil unless caption_exists?
+
+    File.read(caption_path)
+  end
+end
